e0ff82994fe3530b1c3a63ed5f0d9ac1b6940ede
Niklas Lantau <niklaslantau@gmail.com>
2022-04-20 18:56:02 +0200

    Added logging

--- pwm_demo/test/serial_com2.py
+++ pwm_demo/test/serial_com2.py
@@ -2,17 +2,22 @@
 # nlantau, 2022-04-19
 # updated, 2022-04-20
 
+import logging
 import re
 import serial
 import time
 import threading
 import tkinter as tk
 from tkinter import ttk
+import time
 
 
 class App(tk.Tk):
 
     def __init__(self):
+        log_name = f'{time.strftime("%Y-%m-%d", time.localtime())}'
+        logging.basicConfig(filename=f'{log_name}.log', level=logging.DEBUG)
+        logging.info(f"Application started {time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())}")
         super().__init__()
         self.title('Serial UART - LED control')
         self.geometry('400x200+50+50')
@@ -29,6 +34,8 @@ class MainFrame(ttk.Frame):
         self.parent = container
         self._is_started = False
         self.ser_th = None
+        self._float_regex = re.compile(r'([0-9]{1,7})(\.[0-9]+)?')
+        self._int_regex = re.compile(r'[0-9]{1,2}')
         
         options = {'padx': 5, 'pady':5}
 
@@ -96,21 +103,18 @@ class MainFrame(ttk.Frame):
         self.grid(**options)
 
     def _entry_tracer(self, *args):
-        com1 = re.compile(r'([0-9]{1,7})(\.[0-9]+)?')
         var_len = len(self.var_inp.get())
-        if 7 > var_len > 0 and com1.fullmatch(str(self.var_inp.get())):
-            print(f"in - check, {self.var_inp.get()=}")
+        if 7 > var_len > 0 and self._float_regex.fullmatch(str(self.var_inp.get())):
+            logging.debug(f"in - check, {self.var_inp.get()=}")
             self._ent_valid = self.var_inp.get()
         else:
-            print(f"in - not valid, {self.var_inp.get()=}")
+            logging.debug(f"in - not valid, {self.var_inp.get()=}")
             self._ent_valid = ""
 
     def _entry_tracer_st(self, *args):
-        import re
-        com = re.compile(r'[0-9]{1,2}')
         var_len = len(self.var_inp_st.get())
-        if 3 > var_len > 0 and com.fullmatch(self.var_inp_st.get()):
-            print("in")
+        if 3 > var_len > 0 and self._int_regex.fullmatch(self.var_inp_st.get()):
+            logging.debug("in")
             self._ent_valid_st = self.var_inp_st.get()
         else:
             self._ent_valid_st = ""
@@ -122,6 +126,7 @@ class MainFrame(ttk.Frame):
             time.sleep(1)
             self.parent.destroy()
         self.parent.destroy()
+        logging.info(f"Application stopped {time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())}")
 
 
     def button_clicked_start(self):
@@ -129,12 +134,12 @@ class MainFrame(ttk.Frame):
             if (len(self._ent_valid) > 0 
                 and len(self._ent_valid_st) > 0
                 and not self.iv_c1.get()):
-                print(f'{self._ent_valid=}')
+                logging.debug(f'{self._ent_valid=}')
                 self.ser_th = SerialReader(self, sleep=self._ent_valid, step=int(self._ent_valid_st), top=255)
                 self.ser_th.start()
                 self._is_started = True
             elif len(self.selected_size.get()) > 0 and len(self.selected_step.get()) > 0 and self.iv_c1.get():
-                print(f'{self._ent_valid=}')
+                logging.debug(f'{self._ent_valid=}')
                 self.ser_th = SerialReader(self, sleep=self.selected_size.get(), step=int(self.selected_step.get()), top=255)
                 self.ser_th.start()
                 self._is_started = True
@@ -163,13 +168,16 @@ class SerialReader(threading.Thread):
         self.ser = serial.Serial(self.PORT, self.BAUD, parity=serial.PARITY_EVEN, stopbits=2, timeout=0.5)
 
     def run(self):
+        logging.info(f"Starting {self._transceive_th.name}")
         self._transceive_th.start()
 
     def stop(self):
         self._do_run = False
         self.ser.cancel_read()
         self.ser.cancel_write()
+        logging.info(f"Joining {self._transceive_th.name}")
         self.join()
+        logging.info(f"Closing {self.ser.name}")
         self.ser.close()
 
     def transceive(self):
@@ -180,10 +188,10 @@ class SerialReader(threading.Thread):
 
             except serial.SerialException as e:
                 if self._do_run:
-                    print(e)
+                    logging.error(e)
             except Exception:
                 pass
-        print("Canceled")
+        logging.info("Stop running")
 
     def _runner(self, start, stop, step, sleep):
         for i in range(start, stop, step):
